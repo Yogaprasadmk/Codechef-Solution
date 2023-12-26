843b5222f2e5e39a2742bd636bac3a52663729fa
Yogaprasad <120255515+Yogaprasadmk@users.noreply.github.com>
2023-12-26 21:50:10 +0530

    Greater Average.java

---
+++ Greater Average.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt(); //for No. of test cases
+		while(T-->0) {
+		    //your code goes here
+		    int a = in.nextInt();
+		    int b = in.nextInt();
+		    int c = in.nextInt();
+		    System.out.println(0.5*(a+b)>c?"Yes":"No");
+		}
+	}
+}
