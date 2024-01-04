0b431920d1413cd84c3a01652e0ef1a04ce71357
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-01-04 19:05:49 +0530

    Police and Thief
    Signed-off-by: Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>

---
+++ Police and Thief.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner PT  = new Scanner(System.in);
+		int t = PT.nextInt();
+		while(t-->0){
+		  int x  = PT.nextInt();
+		  int y = PT.nextInt();
+		  System.out.println(Math.abs(x-y));
+		}
+		
+
+	}
+}
