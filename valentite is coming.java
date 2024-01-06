9afb23c019991b811da15be559f95ed9fd25c686
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-01-06 22:52:40 +0530

    valentite is coming
    Signed-off-by: Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>

---
+++ valentite is coming.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner VC  = new Scanner(System.in);
+		int t = VC.nextInt();
+		while(t-->0){
+		  int x = VC.nextInt();
+		  int y = VC.nextInt();
+		  System.out.println(x/y);
+		}
+
+	}
+}
