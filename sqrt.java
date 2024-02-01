4ef3ff790c2fa29c63910fd55622b0752c1707d0
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-02-01 22:51:50 +0530

    Create sqrt.java
    Signed-off-by: Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>

---
+++ sqrt.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner SR = new Scanner(System.in);
+		int t = SR.nextInt();
+		while(t-->0){
+		  double n = SR.nextInt();
+		  System.out.println((int)Math.sqrt(n));
+		}
+	}
+}
